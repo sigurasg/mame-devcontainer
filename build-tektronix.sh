394c509f63cfb42e778cefd70ec1b7f45fea604d
Sigurdur Asgeirsson <siggi@undo.com>
2023-01-05 20:16:25 +0000

    More build config, build in DEBUG with SYMBOLS.

--- build-tektronix.sh
+++ build-tektronix.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
+export DEBUG=1
+export SYMBOLS=1
 export LDFLAGS=-lfontconfig 
 
+time \
 make -C mame SUBTARGET=tektronix SOURCES=src/mame/tektronix TOOLS=1 -j5
