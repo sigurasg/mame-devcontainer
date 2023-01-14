2aa28992e3bdd9f160e7f20e25372bc071c5561f
Sigurdur Asgeirsson <siggi@undo.com>
2023-01-14 17:53:14 +0000

    Make a start on writing a layout file, by refactoring some primitives to return strings.

--- write_layout.py
+++ write_layout.py
@@ -11,49 +11,53 @@ def rr(x, y, width, height, r, stroke_width):
         style="fill:rgb(255,255,255);stroke-width:{stroke_width}mm;stroke:rgb(0,0,0)"/>"""
 
 
-    print(TEMPL.format(
+    return TEMPL.format(
                     x = x + stroke_width / 2,
                     y = y + stroke_width / 2,
                     width = width - stroke_width,
                     height = height - stroke_width,
                     r = r,
-                    stroke_width=stroke_width))
+                    stroke_width=stroke_width)
 
 
 def main_bezel(width, height, stroke_width):
     # Main bezel.
-    rr(x = 0, y = 0,
+    strs = []
+    strs.append(rr(x = 0, y = 0,
         width = width,
         height = height,
         r = 10,
-        stroke_width = stroke_width)
+        stroke_width = stroke_width))
 
     x = 6.25
     y = 0
-    rr(x = x, y = y,
+    strs.append(rr(x = x, y = y,
         width = 135.6,
         height = 145,
         r = 5,
-        stroke_width = stroke_width)
+        stroke_width = stroke_width))
     x += 7.5
     y += 12.3
-    rr(x = x, y = y,
+    strs.append(rr(x = x, y = y,
         width = 120,
         height = 105.6,
         r = 5,
-        stroke_width = stroke_width)
+        stroke_width = stroke_width))
     x += (120 - 105) / 2
     y += 10
-    rr(x = x,
+    strs.append(rr(x = x,
         y = y,
         width = 105,
         height = 83.2,
         r = 0.1,
-        stroke_width = 1)
+        stroke_width = 1))
+
+    return '\n'.join(strs)
 
 def pot(x, y, name):
-    circle(cx=x, cy=y, r=9.5 / 2, stroke_width=1)
-    circle(cx=x, cy=y, r=5.4 / 2, stroke_width=1)
+    return '\n'.join([
+        circle(cx=x, cy=y, r=9.5 / 2, stroke_width=1),
+        circle(cx=x, cy=y, r=5.4 / 2, stroke_width=1)])
 
 
 def but(x, y, name):
@@ -64,20 +68,22 @@ def but(x, y, name):
 
 def pow(x, y, name):
     # TODO(siggi): Render the power button properly
-    circle(cx = x, cy = y, r = 10 / 2, stroke_width = 0.5)
+    return circle(cx = x, cy = y, r = 10 / 2, stroke_width = 0.5)
 
 
 def volts_div(x, y):
     # TODO(siggi): Render the volts/div dials properly
-    circle(cx = x, cy = y, r = 17.6 / 2, stroke_width = 0.5)
-    circle(cx = x, cy = y, r = 10 / 2, stroke_width = 0.5)
+    return '\n'.join([
+        circle(cx = x, cy = y, r = 17.6 / 2, stroke_width = 0.5),
+        circle(cx = x, cy = y, r = 10 / 2, stroke_width = 0.5)])
 
 
 def sec_div(x, y):
     # TODO(siggi): Render the sec/div dial properly
-    circle(cx = x, cy = y, r = 28.3 / 2, stroke_width = 0.1)
-    circle(cx = x, cy = y, r = 21.3 / 2, stroke_width = 0.5)
-    circle(cx = x, cy = y, r = 10 / 2, stroke_width = 0.5)
+    return '\n'.join([
+        circle(cx = x, cy = y, r = 28.3 / 2, stroke_width = 0.1),
+        circle(cx = x, cy = y, r = 21.3 / 2, stroke_width = 0.5),
+        circle(cx = x, cy = y, r = 10 / 2, stroke_width = 0.5)])
 
 
 def up_dn_switch(x, y, name):
@@ -87,9 +93,10 @@ def up_dn_switch(x, y, name):
 
 
 def bnc(x, y, name):
-    circle(cx = x, cy = y, r = 14.8 / 2, stroke_width = 1)
-    circle(cx = x, cy = y, r = 9.6 / 2, stroke_width = 0.5)
-    circle(cx = x, cy = y, r = 4.7 / 2, stroke_width = 1)
+    return '\n'.join([
+        circle(cx = x, cy = y, r = 14.8 / 2, stroke_width = 1),
+        circle(cx = x, cy = y, r = 9.6 / 2, stroke_width = 0.5),
+        circle(cx = x, cy = y, r = 4.7 / 2, stroke_width = 1)])
 
 
 def led(x, y, name):
@@ -100,7 +107,7 @@ def circle(cx, cy, r, stroke_width):
     CIRCLE_TEMPL = """<circle cx="{cx}mm" cy="{cy}mm" r="{r}mm"
         style="fill:rgb(255,255,255);stroke-width:{stroke_width}mm;stroke:rgb(0,0,0)"/>"""
 
-    print(CIRCLE_TEMPL.format(cx = cx, cy = cy, r = r - stroke_width / 2, stroke_width = stroke_width))
+    return CIRCLE_TEMPL.format(cx = cx, cy = cy, r = r - stroke_width / 2, stroke_width = stroke_width)
 
 
 def center_rect(x, y, width, height, stroke_width):
@@ -122,15 +129,15 @@ def main():
     print(XML_HEADER)
     print(SVG_HEADER.format(width = bezel_width, height = bezel_height))
 
-    main_bezel(width = bezel_width, height = bezel_height, stroke_width = 3)
+    print(main_bezel(width = bezel_width, height = bezel_height, stroke_width = 3))
 
     # Pots and buttons under the CRT.
     first_pot = 4.41 + 6.2 + 5
     pot_space = 30.3
-    pot(x = first_pot + 0 * pot_space, y = 134, name = "INTENSITY")
-    pot(x = first_pot + 1 * pot_space, y = 134, name = "FOCUS")
-    pot(x = first_pot + 2 * pot_space, y = 134, name = "READOUT INTENSITY")
-    pot(x = first_pot + 3 * pot_space, y = 134, name = "SCALE ILLUM")
+    print(pot(x = first_pot + 0 * pot_space, y = 134, name = "INTENSITY"))
+    print(pot(x = first_pot + 1 * pot_space, y = 134, name = "FOCUS"))
+    print(pot(x = first_pot + 2 * pot_space, y = 134, name = "READOUT INTENSITY"))
+    print(pot(x = first_pot + 3 * pot_space, y = 134, name = "SCALE ILLUM"))
 
     but(x = first_pot + 0.5 * pot_space, y = 134, name = "BEAM\nFIND")
     pow(x = 4.41 + 6.2 + 117, y = 134, name = "POWER")
@@ -142,16 +149,16 @@ def main():
 
     # Top row pots and buttons
     # CH1 POS
-    pot(x = first_pot + 0 * pot_space, y = 20, name = "POSITION")
+    print(pot(x = first_pot + 0 * pot_space, y = 20, name = "POSITION"))
     # CH2 POS
-    pot(x = first_pot + 1 * pot_space, y = 20, name = "POSITION")
+    print(pot(x = first_pot + 1 * pot_space, y = 20, name = "POSITION"))
     # VERT POS
     vert_pos_x = first_pot + 2 * pot_space
-    pot(x = vert_pos_x, y = 20, name = "POSITION")
+    print(pot(x = vert_pos_x, y = 20, name = "POSITION"))
     but(x = vert_pos_x + 16, y = 20, name = "X10 MAG")
 
-    pot(x = first_pot + 88, y = 20, name = "HOLDOFF")
-    pot(x = first_pot + 120, y = 20, name = "LEVEL")
+    print(pot(x = first_pot + 88, y = 20, name = "HOLDOFF"))
+    print(pot(x = first_pot + 120, y = 20, name = "LEVEL"))
 
     led_x = first_pot + (88 + 120) / 2
     led_space = 4
@@ -185,16 +192,16 @@ def main():
     but(x = first_but + 3 * but_space, y = row_y, name ="20 MHZ\nBW LIMIT")
 
     row_y = 52
-    pot(x = bezel_edge + 72, y = row_y, name = "D REF or DLY POS")
-    pot(x = bezel_edge + 72 + 22.3, y = row_y, name = "D")
+    print(pot(x = bezel_edge + 72, y = row_y, name = "D REF or DLY POS"))
+    print(pot(x = bezel_edge + 72 + 22.3, y = row_y, name = "D"))
 
     btn_width = 18.1
     first_but = bezel_edge + 7.5 + btn_width / 2
     row_y = 87.1 - btn_width / 2
-    volts_div(x = first_but, y = row_y)
-    volts_div(x = first_but + 31.6, y = row_y)
+    print(volts_div(x = first_but, y = row_y))
+    print(volts_div(x = first_but + 31.6, y = row_y))
 
-    sec_div(x = first_but + 86.5 - btn_width / 2 - 21.5 / 2, y = row_y)
+    print(sec_div(x = first_but + 86.5 - btn_width / 2 - 21.5 / 2, y = row_y))
 
     first_switch = bezel_edge + 110
     row_y = 83.5
@@ -245,21 +252,21 @@ def main():
     led(x = led_x, y = 94 + 3 * led_space, name = "GND")
     led(x = led_x, y = 94 + 4 * led_space, name = "50Ohm DC")
 
-    pot(x = bezel_edge + 72, y = row_y, name = "TRACE\nSEP")
+    print(pot(x = bezel_edge + 72, y = row_y, name = "TRACE\nSEP"))
     # CH3 POS
-    pot(x = bezel_edge + 96, y = row_y, name = "POSITION")
+    print(pot(x = bezel_edge + 96, y = row_y, name = "POSITION"))
 
     but(x = bezel_edge + 111, y = row_y, name = "VOLTS/DIV")
     but(x = bezel_edge + 121, y = row_y, name = "VOLTS/DIV")
 
     # CH4 POS
-    pot(x = bezel_edge + 139, y = row_y, name = "POSITION")
+    print(pot(x = bezel_edge + 139, y = row_y, name = "POSITION"))
 
     row_y = 130
-    bnc(x = bezel_edge + 16, y = row_y, name = "CH 1")
-    bnc(x = bezel_edge + 58, y = row_y, name = "CH 2")
-    bnc(x = bezel_edge + 95, y = row_y, name = "CH 3")
-    bnc(x = bezel_edge + 139, y = row_y, name = "CH 4")
+    print(bnc(x = bezel_edge + 16, y = row_y, name = "CH 1"))
+    print(bnc(x = bezel_edge + 58, y = row_y, name = "CH 2"))
+    print(bnc(x = bezel_edge + 95, y = row_y, name = "CH 3"))
+    print(bnc(x = bezel_edge + 139, y = row_y, name = "CH 4"))
 
     print(SVG_TRAILER)
 
